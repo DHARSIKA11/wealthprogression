2996cbf2be491ff831051adb868f1ddc1c45b3fe
DHARSIKA S <83574724+DHARSIKA11@users.noreply.github.com>
2022-05-07 22:47:43 +0530

    Add files via upload

---
+++ wealthprogression.py
@@ -0,0 +1,13 @@
+st.write(f" " "
+ ## Experiment Parameters
+ * Initial Wealth = ${sl_initial _wealth}
+ * Faster Growth Percentage = {sl_fast_growth_pct)%    
+ * Slower Growth Percentage ={sl_slow_growth_pct)%
+ * Time Steps = 60
+ * Number of Individuals = 1000
+ * Probability of Faster Growth = {sl_probability_faster_growth}
+  " " ")
+
+ if st.sidebar.button("Run Experiment"):
+         run_experiment(sl_initial_wealth,sl_fast_growth_pct,sl_slow_growth_pct)
+         
