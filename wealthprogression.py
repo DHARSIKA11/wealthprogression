9f645329970c13128e19179fa84e651479a05f46
DHARSIKA S <83574724+DHARSIKA11@users.noreply.github.com>
2022-05-07 22:55:35 +0530

    Update wealthprogression.py

--- wealthprogression.py
+++ wealthprogression.py
@@ -1,4 +1,4 @@
-st.write(f" " "
+st.write(f"""
  ## Experiment Parameters
  * Initial Wealth = ${sl_initial _wealth}
  * Faster Growth Percentage = {sl_fast_growth_pct)%    
@@ -6,7 +6,7 @@ st.write(f" " "
  * Time Steps = 60
  * Number of Individuals = 1000
  * Probability of Faster Growth = {sl_probability_faster_growth}
-  " " ")
+  """)
 
  if st.sidebar.button("Run Experiment"):
          run_experiment(sl_initial_wealth,sl_fast_growth_pct,sl_slow_growth_pct)
